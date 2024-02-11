3ba803c668d523bc6207023284e151838e5e8486
Elzbieta <elzbietasousafabri@gmail.com>
2024-02-10 21:57:43 -0300

    Alter estilos e html, implementei as funcionalidades finais

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-"# one-challenge-cipherword-decryptor" 
+<h1>
+    <a href="https://cursos.alura.com.br/">
+     <img align="center" width="40px" src="./assets/images/logo.svg"></a>
+    <span>Desafio: Cipherword Decryptor</span>
+</h1>
+
+## 📚Confira meu caderno de estudos:
+[![portfolio](https://img.shields.io/badge/Caderno_de_Estudos_-_ORACLE-E30613?style=for-the-badge&logo=ko-fi&logoColor=white)](https://elzbieta.notion.site/elzbieta/PLANNER-ORACLE-2502335c340d450cbc04e55458f9ce75)
+
+## 📝 Ferramentas utilizadas no Bootcamp
+- NOTION
+- GIT e GITHUB
+- JAVASCRIPT
+- FIGMA
+
+
+<table>
+  <thead>
+    <tr align="left">
+      <th>Nº</th>
+      <th>Status</th>
+      <th>Etapas</th>
+      <th>Projetos</th>
+    </tr>
+  </thead>
+  <tbody align="left">
+    <tr>
+      <td>01</td>
+      <td><img width="30px" height="30px" align="center" alt="icon check" src="./assets/images/check.png"></td>
+      <td>Organização do desafio no Notion</td>
+      <td align="center">
+        <a href="https://elzbieta.notion.site/PLANNER-ORACLE-2502335c340d450cbc04e55458f9ce75" target="_blank">
+           <img align="center" alt="Botao" src="https://img.shields.io/badge/Ver%20Notion-132123?style=for-the-badge" width="150px">
+        </a>
+      </td>
+    </tr>
+    <tr>
+      <td>02</td>
+      <td><img width="30px" height="30px" align="center" alt="icon check" src="./assets/images/check.png"></td>
+      <td>Projeto Finalizado! Confira 🎈🎉✨</td>
+      <td align="center">
+        <a href="https://github.com/elizabetesfabri/bootcamp-oracle/logica-programacao" target="_blank">
+           <img align="center" alt="Botao" src="https://img.shields.io/badge/Ver%20Site-E30613?style=for-the-badge" width="150px">
+        </a>
+      </td>
+    </tr>
+    </tbody>
+  <tfoot></tfoot>
+</table>
+
+![Gif do projeto](./assets/images/cripto.gif)
+
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"><br>
+
+##### Desenvolvido por <span>Elizabete Fabri</span> 🧡 
---
+++ assets/scripts/functions.js
@@ -0,0 +1,88 @@
+let textoEntrada = "";
+let textoSaida = "";
+let botaoCopiar = "";
+const regex = "[^a-z ]";
+
+document.getElementById('copiar__texto').style.display = 'none';
+
+function criptografar() {
+    let resultado = document.getElementById('resultado');
+    textoEntrada = document.querySelector('textarea').value;
+
+    textoSaida = textoEntrada.replace(/e/g, "enter")
+                                .replace(/i/g, "imes")
+                                .replace(/a/g, "ai")
+                                .replace(/o/g, "ober")
+                                .replace(/u/g, "ufat");
+
+    botaoCopiar = textoSaida;           
+    resultado.innerHTML = textoSaida;
+
+    document.getElementById('mobile_info').style.display = 'none';
+    document.getElementById('copiar__texto').style.display = 'block';
+}
+
+function descriptografar() {
+    let resultado = document.getElementById('resultado');
+    textoEntrada = document.querySelector('textarea').value;
+
+    textoSaida = textoEntrada.replace(/enter/g, "e")
+                                .replace(/imes/g, "i")
+                                .replace(/ai/g, "a")
+                                .replace(/ober/g, "o")
+                                .replace(/ufat/g, "u");
+
+    botaoCopiar = textoSaida;
+    resultado.innerHTML = textoSaida;
+
+    document.getElementById('mobile_info').style.display = 'none';
+    document.getElementById('copiar__texto').style.display = 'block';
+}
+
+function copiarCriptografia() {
+    navigator.clipboard.writeText(botaoCopiar)
+        .then(() => {
+            alert('Texto copiado com sucesso!');
+            limparTela(); 
+        })
+        .catch(err => {
+            console.error('Erro ao copiar o texto:', err);
+        });
+    
+}
+
+
+function limparTela() {
+    document.querySelector('textarea').value = "";
+    document.getElementById('resultado').innerHTML = ""; 
+    document.getElementById('mobile_info').style.display = 'block'; 
+    document.getElementById('copiar__texto').style.display = 'none'; 
+    document.getElementById('exclamation').style.color = '#495057'; 
+}
+function validacaoCriptografia(){
+    textoEntrada = document.querySelector('textarea').value;
+ 
+    if (!textoEntrada.match(regex)) {
+        document.getElementById('exclamation').style.color = '#495057'; 
+        criptografar();   
+    } else {
+        document.getElementById('resultado').innerHTML = "";
+        document.getElementById('mobile_info').style.display = 'block';
+        document.getElementById('copiar__texto').style.display = 'none';
+        document.getElementById('exclamation').style.color = '#e63636'; 
+    }
+}
+
+function validacaoDescriptografia() {
+    textoEntrada = document.querySelector('textarea').value;
+ 
+    if (!textoEntrada.match(regex)) {
+        document.getElementById('exclamation').style.color = '#495057'; 
+       descriptografar();
+    } else {
+        document.getElementById('resultado').innerHTML = "";
+        document.getElementById('mobile_info').style.display = 'block';
+        document.getElementById('copiar__texto').style.display = 'none';
+        document.getElementById('exclamation').style.color = '#e63636'; 
+    }
+}
